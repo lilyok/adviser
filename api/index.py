eafb733b1757d128530ab6f20c50f6a3e4550670
lilyok <lilyoknabieva@gmail.com>
2021-05-18 18:48:54 +0100

    add motivational quotes

--- api/index.py
+++ api/index.py
@@ -9,14 +9,17 @@ PLANS_PATH = 'ready_made_plans'
 @app.route('/<path:path>')
 async def index(request, path=""):
     topic = request.args.get('topic') or 'topic_list'
-    sort_func = lambda data: (sorted(data, key=lambda x: x['title']))
 
     try:
-        with open(f'{PLANS_PATH}/{topic}.json', mode='r') as json_file:
-            data = json.load(json_file)
-            return sjson({'data': sort_func(data)})
-
-    except FileNotFoundError:
-        return sjson({'data': []})
+        sort_func = lambda data: (sorted(data, key=lambda x: x['title']))
+        try:
+            with open(f'{PLANS_PATH}/{topic}.json', mode='r') as json_file:
+                data = json.load(json_file)
+                return sjson({'data': sort_func(data)})
+        except FileNotFoundError:
+            return sjson({'data': []})
+    
+    except KeyError:
+        return sjson(data)
 
 #  sanic api.index.app
